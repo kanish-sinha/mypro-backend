f8aed64dbe8a2b71a829896b98bd5c03a3e39c3a
kanish <kanish@rudrainnovative.in>
2022-01-10 15:17:54 +0530

    messagerout

--- routs/messagerout.js
+++ routs/messagerout.js
@@ -6,7 +6,11 @@ router.get('/allmsg', async(req, res) => {
     res.json(msg);
 })
 router.get('/msg/:sender/:receiver', async(req, res) => {
-    let msg = await Message.find({ $or: [{ sender: req.params.sender }, { receiver: req.params.receiver }, { sender: req.params.receiver }] });
+    let msg = await Message.find({ $or: [{ sender: req.params.sender }, { receiver: req.params.receiver }] });
+    let msg2 = await Message.find({ $or: [{ sender: req.params.receiver }, { receiver: req.params.sender }] })
+    for (let i = 0; i < msg2.length(); i++) {
+        msg = msg.push(msg2[i])
+    }
     res.json(msg);
 })
 module.exports = router;
