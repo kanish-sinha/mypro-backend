f6b4efe01b7789af59baf6b5098b7552df226076
kanish <kanish@rudrainnovative.in>
2022-01-12 13:20:35 +0530

    populate

--- routs/postrout.js
+++ routs/postrout.js
@@ -6,7 +6,7 @@ router.get('/', async(req, res) => {
     res.json(post);
 })
 router.get('/one/:id', async(req, res) => {
-    let post = await Post.find({ _id: req.params.id })
+    let post = await Post.find({ _id: req.params.id }).populate('user')
     res.json(post);
 })
 router.get('/user/:id', async(req, res) => {
